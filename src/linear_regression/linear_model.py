a2167552b2afd51790df10a32a729cccf8b47fd0
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2024-12-15 17:06:16 +0800

    Update linear_model.py

--- src/linear_regression/linear_model.py
+++ src/linear_regression/linear_model.py
@@ -1,7 +1,9 @@
-from sklearn.linear_model import LinearRegression
+X = df['valence'].values
+y = df['danceability'].values
 
-def train_linear_model(X, y):
-    model = LinearRegression()
-    X_train = X_train.reshape(-1, 1)
-    model.fit(X_train,y_train)
-    return model
+# Creating linear model
+model = LinearRegression()
+
+# Reshape X_train to be a 2D array
+X_train = X_train.reshape(-1, 1)
+model.fit(X_train,y_train)
