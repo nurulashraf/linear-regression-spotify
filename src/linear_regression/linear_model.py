4a075ed572abe13904b93344248703c93d176c8a
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2024-12-16 00:29:50 +0800

    Delete src/linear_regression directory

--- src/linear_regression/linear_model.py
+++
@@ -1,9 +0,0 @@
-X = df['valence'].values
-y = df['danceability'].values
-
-# Creating linear model
-model = LinearRegression()
-
-# Reshape X_train to be a 2D array
-X_train = X_train.reshape(-1, 1)
-model.fit(X_train,y_train)
