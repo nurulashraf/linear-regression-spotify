aad94f94c94a4c7c8c783c4dc4839ac3bd5e77bd
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2024-12-15 17:03:00 +0800

    Update data_preprocessing.py

--- src/utils/data_preprocessing.py
+++ src/utils/data_preprocessing.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def load_data(filepath):
-    data = pd.read_csv(/content/drive/MyDrive/datasets/spotify_tracks.csv)
-    return data
+df = pd.read_csv('/content/drive/MyDrive/datasets/spotify_tracks.csv')
 
-def preprocess_data(data):
-    # Example: drop null values or encode categorical variables
-    data = data.dropna()
-    return data
+df.head()
+
+df.info()
